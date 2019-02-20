a0acb4e6258694d81dc8b31e55218d0abfa1c0a3
Jonathan <jonathan.hilgart@gmail.com>
2019-02-20 14:40:14 -0800

    working on routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,50 @@
 import { Component } from '@angular/core';
+// import { IProduct } from './product';
+// import { ProductService } from './products.service';
+// import { appService } from './app.service';
+// import { Http , Response } from '@angular/http';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+//
+// @Component({
+//   selector: 'my-app',
+//   templateUrl: 'app/app.component.html',
+//   providers: [ProductService],
+// })
 
-@Component({
-  selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+// export class AppComponent {
+//    appTitle: string = 'Welcome';
+//    appStatus: boolean = true;
+//    appList: any[] = [ {
+//       "ID": "1",
+//       "url" : "app/images/Best_Fitness_copy.png"
+//    },
+//
+//    {
+//       "ID": "2",
+//       "url" : "app/images/GA_100_10_Fevals_copy.png"
+//    } ];
+// }
+
+// export   class   AppComponent  {
+//    iproducts: IProduct[];
+//    constructor(private _product: ProductService) {
+//    }
+//
+//    ngOnInit() : void {
+//       this._product.getproducts()
+//       .subscribe(iproducts => this.iproducts = iproducts);
+//    }
+// }
+
+
+@Component ({
+   selector: 'my-app',
+   template: `
+   <ul>
+      <li><a [routerLink] = "['/Product']">Product</a></li>
+      <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
+   </ul>
+   <router-outlet></router-outlet>`
 })
-
-export class AppComponent {
-   appTitle: string = 'Welcome';
-   appStatus: boolean = true;
-   appList: any[] = [ {
-      "ID": "1",
-      "url" : "app/images/Best_Fitness_copy.png"
-   },
-
-   {
-      "ID": "2",
-      "url" : "app/images/GA_100_10_Fevals_copy.png"
-   } ];
-}
+export class AppComponent  { }
